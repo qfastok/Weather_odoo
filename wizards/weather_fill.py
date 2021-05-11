8874f3537e33de13967bfd36e960a31114c342b9
Денис Яковлєв <denis.yakovlv@gmail.com>
2021-05-11 18:39:23 +0300

    Added list of lines that have errors, after finishing wizard,city_weather tree view will auto update

--- wizards/weather_fill.py
+++ wizards/weather_fill.py
@@ -20,6 +20,7 @@ class FillWeatherLine(models.TransientModel):
     weather_fill_wizard_line_ids = fields.One2many(
         'weather.fill.wizard.line', 'weather_fill_wizard_id')
     file_parsed = fields.Boolean(default=False)
+    data_uploaded = fields.Boolean(default=False)
 
     def parse_weather_fill_file(self):
         self.ensure_one()
@@ -31,40 +32,34 @@ class FillWeatherLine(models.TransientModel):
             format = "%Y-%m-%d"
             error_description = ''
             can_load = True
-            humidity = ''
-            temperature_c = ''
+
             try:
                 datetime.datetime.strptime(row['date'], format)
             except ValueError:
                 error_description += 'Incorrect date format. '
                 can_load = False
-            try:
-                humidity = float(row['humidity'])
-            except ValueError:
-                error_description += 'Humidity should be float. '
-                can_load = False
-            try:
-                temperature_c = float(row['temperature_c'])
-            except ValueError:
-                error_description += 'Humidity should be float. '
-                can_load = False
-            if can_load:
-                wizard_line.create({
-                    'weather_fill_wizard_id': self.id,
-                    'city': row['city'],
-                    'humidity': humidity,
-                    'temperature_c': temperature_c,
-                    'date': fields.Date.to_date(row['date']),
-                })
-            else:
-                wizard_line.create({
-                    'weather_fill_wizard_id': self.id,
-                    'error_description': error_description,
-                })
-                raise UserError(
-                    _('You have incorrect data, check Error Description! '
-                      'Error Description: ' + error_description))
 
+            try:
+                float(row['humidity'])
+            except ValueError:
+                error_description += 'Humidity should be float. '
+                can_load = False
+
+            try:
+                float(row['temperature_c'])
+            except ValueError:
+                error_description += 'Humidity should be float. '
+                can_load = False
+
+            wizard_line.create({
+                'weather_fill_wizard_id': self.id,
+                'city': row['city'],
+                'humidity': row['humidity'],
+                'temperature_c': row['temperature_c'],
+                'date': fields.Date.to_date(row['date']),
+                'can_load': can_load,
+                'error_description': error_description,
+            })
 
         self.file_parsed = True
         return {
@@ -74,6 +69,7 @@ class FillWeatherLine(models.TransientModel):
             'res_model': 'weather.fill.wizard',
             'view_id': view.id,
             'target': 'new',
+
         }
 
     # Method upload_weather: will get weather_fill_wizard_line_ids and creates
@@ -85,20 +81,53 @@ class FillWeatherLine(models.TransientModel):
         wizard_line_fill = self.env['city.weather'].sudo()
         ResCity = self.env['res.city'].sudo()
         country_ua = self.env.ref('base.ua')
+        self.env['weather.fill.wizard.line'].sudo()
+        view = self.env.ref('weather.filling_form')
         for line in self.weather_fill_wizard_line_ids:
+            if not line.can_load:
+                continue
+            if wizard_line_fill.search([('city_id.name', '=', line.city),
+                                        ('date', '=', line.date)]):
+                line.can_load = False
+                line.error_description = 'Date and City must be unique'
+                continue
+
             city_id = ResCity.search([('name', '=', line.city)])
             if not city_id:
                 city_id = ResCity.create(
                     {'name': line.city,
                      'country_id': country_ua.id})
+
                 # self.env.ref- посилання по xml.id
             wizard_line_fill.create({
                 'city_id': city_id.id,
-                'humidity': line.humidity,
-                'temperature_c': line.temperature_c,
+                'humidity': float(line.humidity),
+                'temperature_c': float(line.temperature_c),
                 'date': line.date,  # yyyy-mm-dd
             })
 
+            if line.can_load:
+                line.unlink()
+        self.data_uploaded = True
+        return {
+            'type': 'ir.actions.act_window',
+            'view_mode': 'form',
+            'res_id': self.id,
+            'res_model': 'weather.fill.wizard',
+            'view_id': view.id,
+            'target': 'new',
+        }
+
+    def refresh_city_weather(self):
+        city_weather = self.env['city.weather'].sudo()
+        view = self.env.ref('weather.weather_tree_view')
+        return {
+            'type': 'ir.actions.act_window',
+            'view_mode': 'tree',
+            'res_id': city_weather.id,
+            'res_model': 'city.weather',
+            'view_id': view.id,
+        }
 
 class FillWeather(models.TransientModel):
     _name = "weather.fill.wizard.line"
@@ -107,8 +136,8 @@ class FillWeather(models.TransientModel):
     weather_fill_wizard_id = fields.Many2one(
         'weather.fill.wizard', readonly=True)
     city = fields.Char(string='City', readonly=True)
-    temperature_c = fields.Float(string='Temperature C', readonly=True)
-    humidity = fields.Float(string='Humidity', readonly=True)
+    temperature_c = fields.Char(string='Temperature C', readonly=True)
+    humidity = fields.Char(string='Humidity', readonly=True)
     error_description = fields.Char(readonly=True, size=150,
                                     string='Error Description')
     can_load = fields.Boolean(default=True)
